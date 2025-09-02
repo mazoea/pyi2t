248ee7355934e5a9e95a99c6bdd12dd11b5fce87
TooMindoo <tomas.minac@dataquest.sk>
2025-09-02 16:42:57 +0200

    Extend constructor for parser

--- src/pylib_forms.cpp
+++ src/pylib_forms.cpp
@@ -189,7 +189,7 @@ namespace maz {
         ;
 
         py::class_<maz::forms::ib::ub_parser>(m, "ub_parser")
-            .def(py::init<maz::doc::document&>())
+            .def(py::init<maz::doc::document&, const bbox_type&>())
             .def("is", &maz::forms::ib::ub_parser::is)
             .def("report", &maz::forms::ib::ub_parser::report)
             .def("parse", 
